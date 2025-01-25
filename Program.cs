24af31429ff089fd62784c9d0bb07c7dcc81ffe9
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2025-01-25 12:01:55 -0500

    phase 2 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int userChoice;
+int secretNumber = 42;
+
+void DisplayPrompt(){
+
+Console.WriteLine("Welcome To Guessing Game!!");
+
+Console.Write("Guess the Secret Number: ");
+userChoice = int.Parse(Console.ReadLine());
+
+// Console.Write("You Guessed: ");
+// Console.Write(userChoice);
+
+}// close Display Prompt 
+
+DisplayPrompt();
+
+if (secretNumber == userChoice)
+{
+  Console.WriteLine("You Guessed The Secret Number!!");
+} else {
+  Console.WriteLine("Wrong!");
+} //close if else
